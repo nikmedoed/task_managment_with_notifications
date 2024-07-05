aad27ea343742fd7b13948044c1da6c4623249d0
Nikita Muromtsev <grafniksan@gmail.com>
2024-07-05 14:48:36 +0400

    titles fix

--- webapp/deps.py
+++ webapp/deps.py
@@ -3,14 +3,15 @@ from database import async_dbsession
 from sqlalchemy.ext.asyncio import AsyncSession
 from shared.app_config import app_config
 
-from .titles import CustomJinja2Templates
 from .utils.RedisStore import RedisTokenManager
+
+from fastapi.templating import Jinja2Templates
 
 redis = RedisTokenManager(**app_config.redis.dict(), jwt_secret_key=app_config.telegram.jwt_secret_key)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = CustomJinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 
 async def get_db() -> AsyncSession:
--- webapp/titles.py
+++ webapp/titles.py
@@ -1,7 +1,4 @@
-from starlette.responses import Response
-from starlette.templating import Jinja2Templates
-
-from webapp import templates
+from webapp.deps import templates
 
 SYSTEM_NAME = "Прайм контроль"
 
@@ -22,18 +19,6 @@ def get_title(path: str) -> str:
             return title
     return SYSTEM_NAME
 
-class CustomTemplateResponse(Response):
-    media_type = "text/html"
 
-    def __init__(self, template_name: str, context: dict, *args, **kwargs):
-        request = context.get("request")
-        if request:
-            context["title"] = context.get("title") or get_title(request.url.path)
-        template = templates.get_template(template_name)
-        content = template.render(context)
-        super().__init__(content, *args, **kwargs)
-
-
-class CustomJinja2Templates(Jinja2Templates):
-    def TemplateResponse(self, name: str, context: dict, *args, **kwargs):
-        return CustomTemplateResponse(name, context, *args, **kwargs)
+templates.env.globals['get_title'] = get_title
+templates.env.globals['SYSTEM_NAME'] = SYSTEM_NAME
